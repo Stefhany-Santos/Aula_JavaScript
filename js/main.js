04a81f48caf86bc11d5e76201134e83b9d004137
Stefhany Santos <stefhany.contato@outlook.com>
2021-11-22 23:24:38 -0300

    primeira aula

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Primeiros comandos abordados durante a aula //
+
+
+var nome = "Stefhany Santos "; 
+var idade = 21; 
+var idade2 = 21; 
+var n1 = 5;
+var n2 = 10;
+var frase = "Pizza é melhor que lasanha";
+
+// alert(nome + " tem " + idade + " anos");
+// alert(idade + idade2);
+
+console.log(nome); // com esse comando será possível visualizar através da ferramenta do desenvolvedor 
+                     // como por apertando F12 para visualiazr no navegador browser
+console.log(idade + idade2);
+
+console.log(n1 * n2);
+
+console.log(frase.replace("Pizza", "Strogonoff de frango")); // Usar o " .replace " retorna uma nova cadeia de caracter
+
+alert(frase.replace("Pizza", "Strogonoff de frango")) // Também é possível fazer com o alert
+
+console.log(frase.toUpperCase("Pizza", "Strogonoff de frango")); // Deixa tudo em maiúsculo  
+
+console.log(frase.toLowerCase("Pizza", "Strogonoff de frango")); // Deixa em minúsculo
